7d3971f7b31c23000b5c96bebadb341ef32afb27
Bodhisattwa Basu <basubodhisattwa6@gmail.com>
2024-02-01 18:56:29 +0530

    g-auth

--- Backend/middleware/passport.js
+++ Backend/middleware/passport.js
@@ -1,17 +1,33 @@
-// const passport = require('passport');
-// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/user')
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 // const AppleStrategy = require('passport-apple').Strategy;
 
-// passport.use(new GoogleStrategy({
-//     clientID: YOUR_GOOGLE_CLIENT_ID,
-//     clientSecret: YOUR_GOOGLE_CLIENT_SECRET,
-//     callbackURL: "/auth/google/callback"
-//   },
-//   function(accessToken, refreshToken, profile, cb) {
-//     // Find or create user in your database here
-//     return cb(null, profile);
-//   }
-// ));
+passport.use(new GoogleStrategy({
+    clientID: '799450234572-dc4m8rkk5gkbf00viqds8orb5ncp52h2.apps.googleusercontent.com',
+    clientSecret: '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',
+    callbackURL: "/auth/google/callback"
+  },
+  async function(accessToken, refreshToken, profile, cb) {
+    // Find or create user in your database here
+    const existingUser = await User.findOne({ googleId: profile.id });
+
+    if (existingUser) {
+      return cb(null, existingUser);
+    }
+
+    const newUser = new User({
+      googleId: profile.id,
+      username: profile.displayName,
+      // add any other details you want from profile
+    });
+
+    await newUser.save();
+
+    return cb(null, newUser);
+  }
+));
 
 // passport.use(new AppleStrategy({
 //     clientID: YOUR_APPLE_CLIENT_ID,
--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -2,21 +2,22 @@ const express = require('express');
 const passport = require('passport');
 const authController = require('../controllers/authController');
 const passportSetup = require('../middleware/passport');
+const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
 const router = express.Router();
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
-// router.get('/google',
-//   passport.authenticate('google', { scope: ['profile'] }));
+router.get('/google',
+  passport.authenticate('google', { scope: ['profile'] }));
 
-// router.get('/google/callback', 
-//   passport.authenticate('google', { failureRedirect: '/login' }),
-//   function(req, res) {
-//     const token = jwt.sign({ user: req.user }, YOUR_JWT_SECRET);
-//     res.redirect('/home?token=' + token);
-//   });
+router.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    const token = jwt.sign({ user: req.user }, secret);
+    res.redirect('/home?token=' + token);
+  });
 
 // router.get('/apple',
 //   passport.authenticate('apple'));
@@ -24,7 +25,7 @@ router.post('/login', authController.login);
 // router.post('/apple/callback', 
 //   passport.authenticate('apple', { failureRedirect: '/login' }),
 //   function(req, res) {
-//     const token = jwt.sign({ user: req.user }, YOUR_JWT_SECRET);
+//     const token = jwt.sign({ user: req.user }, secret);
 //     res.redirect('/home?token=' + token);
 //   });
 
