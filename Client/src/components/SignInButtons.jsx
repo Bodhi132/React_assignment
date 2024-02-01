2c6fa8410170b05339e3c2646194bed8f310bd80
Bodhisattwa Basu <basubodhisattwa6@gmail.com>
2024-02-01 19:43:45 +0530

    auth3

--- Client/src/components/SignInButtons.jsx
+++ Client/src/components/SignInButtons.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const SignInButtons = ({ imgUrl, type, onClick }) => {
     const [buttonState, setButtonState] = React.useState("loaded");
-    const onClick = async () => {
+    const handleClick = async () => {
         setButtonState("loading");
         await onClick();
         setButtonState("loaded");
     };
     return (
-        <div onClick={onClick} className=' sm:w-[197.6px] w-[153px] h-[32.93px] bg-[#ffffff] flex justify-around items-center rounded-[10px]'>
+        <div onClick={handleClick} className=' sm:w-[197.6px] w-[153px] h-[32.93px] bg-[#ffffff] flex justify-around items-center rounded-[10px]'>
             <img src={imgUrl} className=' w-[15.37px] h-[15.37px]' />
             <p className='font-montserrat text-[#858585] font-[400] text-[12px]'>{buttonState === "loaded" ? `Sign in with ${type}` : "wait..."}</p>
         </div>
