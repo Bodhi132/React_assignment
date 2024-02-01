68b4f5412f640e93b8672457166cd8948871b6b4
Bodhisattwa Basu <basubodhisattwa6@gmail.com>
2024-02-01 20:19:58 +0530

    auth10

--- Backend/index.js
+++ Backend/index.js
@@ -9,9 +9,14 @@ app.use(express.json());
 app.use(cors({ credentials: true, origin: ['http://localhost:5173','https://react-assignment-frontend.vercel.app','https://react-assignment-frontend-acykept6y-bodhi132.vercel.app','http://localhost:3000']}));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
     next();
   });
+  
   
 
 const uri = "mongodb+srv://bodhi:W7Jy92luuZQgGvNN@cluster0.xanjw9x.mongodb.net/?retryWrites=true&w=majority"
