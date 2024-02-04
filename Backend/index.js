55ec99ed134d7331f3a054a4d58657fefcecb353
Bodhisattwa Basu <basubodhisattwa6@gmail.com>
2024-02-04 22:09:53 +0530

    new cors added

--- Backend/index.js
+++ Backend/index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(cors({
   origin: 'https://react-assignment-frontend.vercel.app', // or '*' to allow all origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 
